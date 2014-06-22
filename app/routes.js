02d7ef4ba5b99e5a7ff418352440a5556e341077
Prescott Murphy <prurph@gmail.com>
2014-06-22 11:33:39 -0400

    Implement Facebook authentication

--- app/routes.js
+++ app/routes.js
@@ -29,6 +29,15 @@ module.exports = function(app, passport) {
     });
   });
 
+  // FACEBOOK ROUTES
+  app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+
+  app.get('/auth/facebook/callback',
+    passport.authenticate('facebook', {
+      successRedirect: '/profile',
+      failureRedirect: '/'
+  }));
+
   app.get('/logout', function(req, res) {
     req.logout();
     res.redirect('/');
--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,7 @@
-var LocalStrategy = require('passport-local').Strategy;
-var User          = require('../app/models/user');
+var LocalStrategy    = require('passport-local').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
+var User             = require('../app/models/user');
+var configAuth       = require('./auth');
 
 // the statement in server.js that requires this file is
 // require(./config/passport.js)(passport), so passport gets passed in
@@ -64,6 +66,38 @@ module.exports = function(passport) {
         return done(null, false, req.flash('loginMessage', 'Oops! Wrong password'));
 
       return done(null, user);
+    });
+  }));
+  // FACEBOOK ==================================================================
+  passport.use(new FacebookStrategy({
+    clientID     : configAuth.facebookAuth.clientID,
+    clientSecret : configAuth.facebookAuth.clientSecret,
+    callbackURL  : configAuth.facebookAuth.callbackURL
+  },
+  function(token, refreshToken, profile, done) {
+    process.nextTick(function() {
+      User.findOne({ 'facebook.id' : profile.id }, function(err, user) {
+        if (err)
+          return done(err);
+
+        if (user) {
+          return done(null, user);
+        } else {
+          var newUser = new User();
+
+          newUser.facebook.id    = profile.id;
+          newUser.facebook.token = token;
+          newUser.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
+          newUser.facebook.email = profile.emails[0].value;
+
+          newUser.save(function(err) {
+            if (err)
+              throw new Error(err);
+
+            return done(null, newUser);
+          });
+        }
+      });
     });
   }));
 };
